d954eb223e83aaf594b459971fc5c71d5db39325
4D Systems <engineering@4dsystems.com.au>
2016-08-17 12:55:43 +1200

    Better Arduino Due Support
    Better Arduino Due Support

--- Arduino/Picaso_Serial_4DLib/Picaso_Serial_4DLib.cpp
+++ Arduino/Picaso_Serial_4DLib/Picaso_Serial_4DLib.cpp
@@ -16,8 +16,11 @@
 
 
 Picaso_Serial_4DLib::Picaso_Serial_4DLib(Stream * virtualPort) { 
-        _virtualPort = virtualPort; 
+    _virtualPort = virtualPort; 
+#ifndef _SAM3XA_
+	// Only done on non Arduino Due Boards
 	_virtualPort->flush();
+#endif
 }
 
 //*********************************************************************************************//
